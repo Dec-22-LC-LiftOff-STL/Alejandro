95c9b7dfb5c82be8b789e9d5f95a8dbf5923fcf7
xSorArc <87716789+xSorArc@users.noreply.github.com>
2022-12-05 14:56:57 -0600

    Initial commit with gradle

---
+++ src/main/java/lifestyle/LifestyleApplication.java
@@ -0,0 +1,12 @@
+package lifestyle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LifestyleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LifestyleApplication.class, args);
+    }
+}
