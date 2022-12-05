2e2c12591d00c7107281fc577fdb8e5a9154d206
xSorArc <87716789+xSorArc@users.noreply.github.com>
2022-12-05 15:19:05 -0600

    Added event files with TODOs included

---
+++ src/main/java/lifestyle/controllers/EventController.java
@@ -0,0 +1,9 @@
+package lifestyle.controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class EventController {
+
+    //TODO: Fill out EventController handlers
+}
