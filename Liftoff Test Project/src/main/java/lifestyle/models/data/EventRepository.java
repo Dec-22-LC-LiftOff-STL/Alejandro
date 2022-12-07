e06a4702a24284885ef298ae31dd7e8a33d1533d
xSorArc <87716789+xSorArc@users.noreply.github.com>
2022-12-06 22:00:14 -0600

    Added links to html pages & added fragment to fragments.html

--- Liftoff Test Project/src/main/java/lifestyle/models/data/EventRepository.java
+++
@@ -1,9 +0,0 @@
-package lifestyle.data;
-
-import lifestyle.models.Event;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EventRepository extends CrudRepository<Event, Integer> {
-}
